1c109fdc311d462d7b645fd2cd0e453df5a26d0a
pat3399 <64986845+pat3399@users.noreply.github.com>
2020-05-11 18:40:56 +0200

    Add files via upload

---
+++ zookeeper.py
@@ -0,0 +1,209 @@
+camel = """
+Switching on camera from habitat with camels...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Yey, our little camel is sunbathing!"""
+
+lion = """
+Switching on camera from habitat with lions...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is croaking!"""
+
+deer = """
+Switching on camera from habitat with deers...
+   /|       |\\
+`__\\\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \\
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!"""
+
+goose = """
+Switching on camera from habitat with lovely goose...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \\
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \\
+ <_  `     (  <`<            \              `-._\\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+This bird stares intently at you... (Maybe it's time to change the channel?)"""
+
+bat = """
+Switching on camera from habitat with bats...
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\\\  W  //           <
+       /             /~---~\             \\
+      /_            |       |            _\\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\\
+                ~-. /  \_/  \ .-~
+                   V         V
+It looks like this bat is fine."""
+
+rabbit = """
+Switching on camera from habitat with rabbits...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \\
+\//\/      .- \\
+ Y        /    Y 
+ l       I     !
+ ]\      _\    /"\\
+(" ~----( ~   Y.  )
+It seems there will be more rabbits soon!"""
+
+bear = """
+Switching on camera from habitat with bears...
+                 _.--"'"-,
+               .'         `\
+              /   _         \
+ .-""-.       |  (O\.--.-.-/O)          .-""-.
+/ O O  \      .\|(_._.__._.__)         /  O O \
+|O .-.  \    (   )   0 _ 0   \        /  .-. O|
+\ (   )  '.   `-|     (_)     |     .'  (   ) /
+ '.`-'     '-./`|             |`\.-'     '-'.'
+   \         |  \   \     /   /  |         /
+    \        \   '.  '._.'  .'   /        /
+     \        '.   `'-----'`   .'        /
+      \   .'    '-._        .-'\   '.   /
+       |/`          `'''''')    )    `\|
+       /                  (    (      ,\
+      ;                    \    '-..-'/ ;
+      |                     '.       /  |
+      |                       `'---'`   |
+      ;                                 ;
+       \                               /
+        `.                           .'
+          '-._                   _.-'
+           __/`"  '  - - -  ' "`` \__
+         /`            /^\           `\
+         \(          .'   '.         )/
+          '.(__(__.-'       '.__)__).'
+It looks like the bear is happy."""
+
+dolphin = """
+Switching on camera from habitat with dolphins...
+                                    _
+                               _.-~~.)
+         _.--~~~~~---....__  .' . .,'
+       ,'. . . . . . . . . .~- ._ (
+      ( .. .g. . . . . . . . . . .~-._
+   .~__.-~    ~`. . . . . . . . . . . -.
+   `----..._      ~-=~~-. . . . . . . . ~-.
+             ~-._   `-._ ~=_~~--. . . . . .~.
+              | .~-.._  ~--._-.    ~-. . . . ~-.
+               \ .(   ~~--.._~'       `. . . . .~-.                ,
+                `._\         ~~--.._    `. . . . . ~-.    .- .   ,'/
+_  . _ . -~\        _ ..  _          ~~--.`_. . . . . ~-_     ,-','`  .
+             ` ._           ~                ~--. . . . .~=.-'. /. `
+       - . -~            -. _ . - ~ - _   - ~     ~--..__~ _,. /   \  - ~
+              . __ ..                   ~-               ~~_. (  `
+)`. _ _               `-       ..  - .    . - ~ ~ .    \    ~-` ` `  `. _
+      _  _
+Our little doplhin plays in the water..."""
+
+monkey = """
+Switching on camera from habitat with monkeys...
+       .--.  .-'''-.  .--.
+      /."".v'.-. .-.`v.""\\
+      ||  / / O| | O\ \  ||
+      \\_/| \__| |__/ |\_//
+       `-'\  .-n-n-.  /`-'
+        _.-\/       \/-._
+      .'   (\`.___.'/)   `.
+     /      \`.___.'/      `.
+    /        `.___.'         \
+    |     |             \     \
+    |     |   .      .  |\     \
+    |     |             | \     \
+     \     \            |  \     \
+      \     \           |.' `.    \
+       `.    \         .'     `.   \
+  _.._   `.   `-. ___ /        /`.  `.
+'    "-._|`\    `.__)       .'  /    `.
+|         `-.\     \/      .'   / /\  )|\.
+ \          _/ / /|/     .'    (_/ / / | \)
+  `._      (__/_/-/   ..'         (_/| |\_)
+     ``--._____.-(     `.            `-'
+                  `--.   `.
+                    (_/\ \\\
+                       /_///
+ Ohh, our Monkey looks hungry. Let's go to feed it"""
+
+animals = [camel, lion, deer, goose, bat, rabbit, bear, dolphin, monkey]
+
+# write your code here
+while True:
+    x = input("Which habitat # do you need?")
+    if x != 'exit':
+        print(animals[int(x)])
+    elif x == 'exit':
+        print("See you!")
+        exit()
